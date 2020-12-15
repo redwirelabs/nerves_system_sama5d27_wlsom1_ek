d157c98c935f14676634096ac437613cca07b3d1
Justin Schneck <jschneck@mac.com>
2020-12-15 10:44:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# ATSAMA5D27-WLSOM1 Evaluation Kit
+
+https://www.microchip.com/DevelopmentTools/ProductDetails/PartNO/DM320117
+
+![ATSAMA5D27-WLSOM1 Evaluation Kit image](assets/images/191126-MPU-PHOTO-ATSAMA5D27-WLSOM1-EK1 Evaluation Kit-Angle-5x7.jpg)
+<br><sup>[Image credit](#microchip)</sup>
+
+| Feature              | Description                     |
+| -------------------- | ------------------------------- |
+| CPU                  | 500 MHz ARM Cortex-A5           |
+| Memory               | 256M DDR2                       |
+| Storage              | MicroSD                         |
+| GPIO, I2C, SPI       | Yes - [Elixir Circuits](https://github.com/elixir-circuits) |
+| UART                 | ttyS0                           |
+| Ethernet             | Yes                             |
+| Video                | Yes                             |
+| Camera Interface     | Yes - Parallel ISC              |
+
---
+++ mix.exs
@@ -0,0 +1,89 @@
+defmodule NervesSystemSAMA5D27WLSOM1EK.MixProject do
+  use Mix.Project
+
+  @github_organization "verypossible"
+  @app :nerves_system_sama5d27_wlsom1_ek
+  @version Path.join(__DIR__, "VERSION")
+           |> File.read!()
+           |> String.trim()
+
+  def project do
+    [
+      app: @app,
+      version: @version,
+      elixir: "~> 1.6",
+      compilers: Mix.compilers() ++ [:nerves_package],
+      nerves_package: nerves_package(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      aliases: [loadconfig: [&bootstrap/1]],
+      docs: [extras: ["README.md"], main: "readme"]
+    ]
+  end
+
+  def application do
+    []
+  end
+
+  defp bootstrap(args) do
+    System.put_env("MIX_TARGET", "bbb")
+    Application.start(:nerves_bootstrap)
+    Mix.Task.run("loadconfig", args)
+  end
+
+  defp nerves_package do
+    [
+      type: :system,
+      artifact_sites: [
+        {:github_releases, "#{@github_organization}/#{@app}"}
+      ],
+      platform: Nerves.System.BR,
+      platform_config: [
+        defconfig: "nerves_defconfig"
+      ],
+      checksum: package_files()
+    ]
+  end
+
+  defp deps do
+    [
+      {:nerves, "~> 1.6", runtime: false},
+      {:nerves_system_br, "1.13.5", runtime: false},
+      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 1.3.0", runtime: false},
+      {:nerves_system_linter, "~> 0.3.0", runtime: false},
+      {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Nerves System - SAMA5D27 WLSOM1 EK Dev Kit
+    """
+  end
+
+  defp package do
+    [
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/#{@github_organization}/#{@app}"}
+    ]
+  end
+
+  defp package_files do
+    [
+      "CHANGELOG.md",
+      "fwup_include",
+      "fwup.conf",
+      "fwup-revert.conf",
+      "LICENSE",
+      "linux-4.4.defconfig",
+      "rootfs_overlay",
+      "mix.exs",
+      "nerves_defconfig",
+      "post-createfs.sh",
+      "README.md",
+      "VERSION"
+    ]
+  end
+end
