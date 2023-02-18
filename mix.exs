5f3e9608b3073b83fd8e8a911ab031e94134ca78
Alex McLain <alex@alexmclain.com>
2023-02-18 14:22:25 -0800

    Update nerves_system_br to 1.22.2

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule NervesSystemSAMA5D27WLSOM1EK.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.5.4 or >= 1.6.0", runtime: false},
-      {:nerves_system_br, "1.21.6", runtime: false},
+      {:nerves_system_br, "1.22.2", runtime: false},
       {:nerves_toolchain_armv7_nerves_linux_gnueabihf, "~> 1.6.1", runtime: false},
       {:nerves_system_linter, "~> 0.4", runtime: false},
       {:ex_doc, "~> 0.29", only: [:dev, :test], runtime: false}
